98a02af64b817dface11898b62587a38b20a5ed1
Nishant Date <nishantdate@gmail.com>
2022-05-11 15:57:18 -0400

    Update quart_example.py

--- quart_example.py
+++ quart_example.py
@@ -4,7 +4,7 @@ from quart import make_response, Quart, render_template, url_for
 
 app = Quart(__name__)
 
-@app.route('/')
+@app.route('/stack_colors')
 async def index():
     result = await render_template('index.html')
     response = await make_response(result)
@@ -17,13 +17,9 @@ async def index():
 
 
 if __name__ == '__main__':
-    ssl_context = ssl.create_default_context(
-        ssl.Purpose.CLIENT_AUTH,
+    app.run(
+        host='0.0.0.0', 
+        port=5000, 
+        certfile='cert.pem', 
+        keyfile='key.pem',
     )
-    ssl_context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
-    ssl_context.set_ciphers('ECDHE+AESGCM')
-    ssl_context.load_cert_chain(
-        certfile='cert.pem', keyfile='key.pem',
-    )
-    ssl_context.set_alpn_protocols(['h2', 'http/1.1'])
-    app.run(host='localhost', port=5000, ssl=ssl_context)
